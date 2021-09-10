bab8d7293b0f0b549a86486857f3d2f778434e16
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2021-09-10 20:21:57 +0200

    Changing logo to assets public

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div className="container px-4">
-                <img style={{width: "50px",  paddingRight: "10px"}} src="./assets/logo.png" className="App-logo" alt="logo" />
+                <img style={{width: "50px",  paddingRight: "10px"}} src={window.location.origin + '/assets/logo.png'} className="App-logo" alt="logo" />
 
                 <a className="navbar-brand" href="#page-top">DxImagenVet</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
@@ -16,7 +16,7 @@ export const Header = () => {
             </div>
         </nav>
         <header className="bg-primary bg-gradient text-white">
-            <div className="container px-4 text-center">
+            <div className="container px-4 text-center" style={{ minHeight: '190px'}}>
                 <h1 className="fw-bolder">Diagnóstico por Imagen Veterinario</h1>
                 <p className="lead">Accede a nuestro software de gestión</p>
                 <a className="btn btn-lg btn-light" href="https://dximagenvet.actualpacs.com/actualpacs/login">Entra aquí</a>
