f002c43a48adfd1e7a3791cabde18fb641e6666b
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2021-09-10 20:09:06 +0200

    Changing logo to assets public

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,7 @@ export const Contact = () => {
             <div className="container px-4">
                 <div className="row gx-4 justify-content-center">
                     <div className="col-lg-8">
-                        <h2>¿Cómo contactar con nostros?</h2>
+                        <h2>¿Cómo contactar con nosotros?</h2>
                         <p className="lead">A continuación te falicitamos las distintas formas de como ponerte en contacto con nosotros por si tienes cualquier tipo de duda.
                         También te agradecemos cualquier tipo de feedback sobre nuestro software de gestión de diagnostico.</p>
                         <ul>
